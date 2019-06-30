730a1dbd4abdce5a80fe8df26844d93f7944862a
Yang09701194 <mustnecessity@gmail.com>
2019-06-30 13:19:24 +0800

    1104  1103

--- YangPracticeLeetCode/YangPracticeLeetCode/Program.cs
+++ YangPracticeLeetCode/YangPracticeLeetCode/Program.cs
@@ -16,8 +16,12 @@ namespace YangPracticeLeetCode
         static void Main(string[] args)
         {
 
+			_1104_PathInZigzagLabelledBinaryTree.Test();
+			
+	        
+	        //_1103_DistributeCandiestoPeople.Test();
 
-	        _1029_BinaryPrefixDivisibleBy5.Test();
+	        //_1029_BinaryPrefixDivisibleBy5.Test();
 
 			//_1023_BinaryStringWithSubstringsRepresenting1ToN.Test();
             
