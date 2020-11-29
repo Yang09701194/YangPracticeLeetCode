956a5465fdee9c0f089d10aaf0df1dbcbec43e44
Yang09701194 <mustnecessity@gmail.com>
2020-11-29 21:26:04 +0800

    1016

--- YangPracticeLeetCode/YangPracticeLeetCode/Solved/_1016_BinaryStringWithSubstringsRepresenting1ToN.cs
+++ YangPracticeLeetCode/YangPracticeLeetCode/Solved/_1016_BinaryStringWithSubstringsRepresenting1ToN.cs
@@ -28,10 +28,27 @@ namespace YangPracticeLeetCode.Solved
 		/// 把範圍縮小   然後給小的可負擔的上界
 		/// 就可以處理了  超過的根本都不用理
 		/// 比較難是在每一步限縮都要很精準的給出說明 
-		/// 
+		///
+		///
+		/// The number 1001 ~ 2000 have 1000 different continuous 10 digits.
+		/// The string of length S has at most S - 9 different continuous 10 digits.
+		/// 連續十個字為一組  len 10 就一組  len 11 兩組 因為可以往又在移一個  12 3組  > S-9
+		///  
+		///	So S <= 1000(這是題目的limit 長度最長1000), the achievable N <= 2000. (因為數字值1000~2000 就有1000種組合了
+		/// 長度1000 頂多 991種   所以長度1000等於是說 N的值最大2000
+		///
+		/// 下面應該是推廣到題目以外的限制  也就是考慮所有可能的S長度limit值   可以說是比題目考慮得更全面  是最完整的考慮
+		/// 不論S長度是多少都可以套用  這不得不說一下厲害了
+		/// So S * 2 is a upper bound for achievable N.
 		/// If N > S * 2, we can return false directly in O(1)
 		/// Note that it's the same to prove with the numbers 512 ~ 1511, or even smaller range.
-		///	N/2 times check, O(S)to check a number in string S.9
+		///	N/2 times check, O(S)to check a number in string S.
+		///
+		/// The overall time complexity has upper bound O(S^2).
+		///
+		///
+		///
+		/// 
 		/// </summary>
 		public class Solution
 		{
