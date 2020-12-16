93bfee8a4d1f392d274ba001b05b0d62d489fac7
Yang09701194 <mustnecessity@gmail.com>
2020-12-16 19:47:08 +0800

    20201216

--- YangPracticeLeetCode/YangPracticeLeetCode/Solved/_5627_StoneGameVII.cs
+++ YangPracticeLeetCode/YangPracticeLeetCode/Solved/_5627_StoneGameVII.cs
@@ -11,16 +11,269 @@ namespace YangPracticeLeetCode.Solved
 
 		public static void Test()
 		{
-			Solution s = new Solution();
+			Solution_V2_Cache_DP3 s = new Solution_V2_Cache_DP3();
 
 			//Console.WriteLine(s.NumPoints());
 
-			Console.WriteLine(s.StoneGameVII(new int[] { 5, 3, 1, 4, 2 }));
+			//Console.WriteLine(s.StoneGameVII(new int[] { 5, 3, 1, 4, 2 }));
+
 			//Console.WriteLine(s.StoneGameVII(new int[] { 7, 90, 5, 1, 100, 10, 10, 2 }));
 			//Console.WriteLine(s.StoneGameVII(new int[] { 3, 5 }));
 
+			Console.WriteLine(s.StoneGameVII(new int[] { 454, 989, 739, 904, 646, 100, 639, 449, 891, 46, 32, 483, 99, 311, 270, 224, 151, 188, 667, 513, 880, 758, 720, 861, 460, 270, 461, 164, 46, 755, 409, 921, 913, 623, 959, 33, 671, 536, 716, 907, 377, 481, 871, 757, 644, 402, 795, 904, 852, 591, 122, 564, 109, 661, 160, 530, 73, 835, 195, 622, 724, 443, 620, 531, 771, 152, 423, 450, 944, 391, 757, 673, 886, 924, 161, 413, 899, 167, 356, 250, 548, 581, 403, 708, 685, 723, 363, 362, 980, 395, 593, 926, 323, 126, 939, 150, 176, 316, 776, 136, 757, 495, 613, 252, 605, 203, 743, 50, 363, 867, 467, 570, 269, 373, 919, 149, 75, 342, 658, 870, 723, 245, 587, 963, 172, 500, 361, 589, 812, 647, 515, 827, 334, 740, 416, 436, 1000, 382, 65, 179, 233, 896, 910, 316, 419, 860, 48, 523, 562, 31, 520, 247, 152, 703, 326, 182, 883, 199, 18, 278, 468, 191, 885, 32, 950, 510, 668, 47, 87, 485, 194, 803, 58, 77, 717, 245, 711, 836, 633, 442, 874, 767, 27, 433, 815, 611, 580, 135, 834, 686, 551, 471, 27, 146, 99, 488, 484, 586, 25, 372, 931, 734, 856, 231, 346, 925, 345, 692, 930, 295, 978, 786, 98, 720, 868, 952, 417, 364, 937, 502, 340, 803, 266, 612, 611, 191, 165, 516, 506, 489, 917, 651, 135, 333, 564, 937, 787, 917, 619, 702, 622, 143, 238, 253, 901, 401, 594, 861, 439, 439, 823, 986, 928, 329, 187, 339, 772, 551, 128, 845, 112, 418, 862, 127, 929, 905, 528, 841, 85, 313, 111, 645, 169, 557, 683, 796, 677, 706, 758, 367, 773, 217, 400, 141, 140, 706, 144, 148, 817, 855, 82, 400, 193, 10, 586, 448, 752, 901, 164, 761, 257, 909, 611, 288, 963, 713, 272, 81, 409, 249, 189, 531, 359, 339, 173, 374, 854, 20, 180, 501, 359, 732, 197, 529, 422, 485, 135, 243, 672, 448, 7, 109, 141, 180, 222, 910, 203, 445, 710, 509, 900, 980, 443, 172, 253, 272, 881, 519, 946, 852, 463, 570, 478, 260, 894, 930, 276, 870, 493, 69, 2, 439, 61, 93, 510, 9, 442, 652, 94, 569, 666, 604, 967, 209, 641, 274, 784, 215, 557, 621, 665, 962, 812, 304, 874, 115, 62, 785, 917, 116, 646, 57, 551, 389, 569, 749, 102, 561, 556, 4, 583, 832, 480, 104, 345, 31, 907, 1000, 234, 129, 487, 483, 432, 491, 239, 550, 381, 641, 565, 939, 18, 541, 484, 828, 7, 904, 757, 283, 808, 46, 764, 784, 115, 674, 170, 402, 952, 413, 616, 454, 623, 212, 864, 459, 729, 680, 134, 600, 834, 922, 674, 266, 21, 772, 622, 172, 169, 611, 527, 25, 78, 178, 919, 505, 796, 445, 476, 944, 710, 733, 484, 866, 175, 121, 778, 47, 269, 981, 755, 569, 586, 403, 364, 831, 602, 547, 294, 51, 681, 302, 319, 370, 573, 184, 691, 953, 236, 248, 364, 636, 846, 763, 503, 29, 623, 744, 141, 410, 79, 309, 227, 127, 268, 861, 165, 644, 451, 127, 491, 223, 880, 236, 251, 978, 622, 702, 63, 349, 448, 579, 637, 672, 416, 302, 222, 544, 412, 821, 787, 17, 37, 906, 186, 599, 767, 233, 78, 191, 535, 909, 551, 727, 992, 297, 137, 838, 881, 58, 860, 36, 726, 953, 245, 330, 261, 261, 118, 427, 632, 599, 431, 162, 409, 718, 102, 693, 131, 716, 988, 245, 266, 778, 752, 189, 446, 927, 91, 924, 974, 701, 50, 974, 132, 769, 580, 98, 970, 669, 40, 258, 948, 764, 789, 455, 709, 893, 76, 75, 502, 116, 829, 692, 120, 807, 207, 654, 399, 19, 620, 100, 289, 533, 658, 911, 563, 575, 705, 923, 736, 822, 712, 322, 73, 63, 96, 54, 690, 456, 866, 243, 72, 178, 718, 172, 826, 822, 862, 305, 401, 308, 315, 930, 852, 294, 913, 772, 697, 462, 169, 940, 870, 693, 454, 668, 226, 458, 735, 25, 864, 164, 864, 417, 449, 460, 623, 517, 941, 504, 661, 186, 836, 907, 175, 437, 5, 37, 872, 625, 950, 13, 92, 58, 998, 205, 417, 107, 480, 116, 787, 369, 751, 202, 725, 228, 933, 702, 895, 757, 100, 851, 411, 691, 526, 899, 537, 858, 480, 289, 773, 187, 636, 556, 717, 656, 789, 506, 720, 739, 327, 33, 487, 10, 139, 995, 487, 811 }));
+
+
 		}
 
+
+
+		/// <summary>
+		/// Approach 3: Optimised Memoization Approach
+		///
+		/// 不過測起來跟V2 都差不多
+		///
+		/// 那大概理解一下新的做法
+		/// 
+		/// </summary>
+		class Solution_App3
+		{
+			int[] prefixSum;
+			int[,] memo;
+
+			private int findDifference(int start, int end, int[] stones)
+			{
+				if (start == end)
+				{
+					return 0;
+				}
+				if (memo[start,end] != 0)
+				{
+					return memo[start,end];
+				}
+				if (start + 1 == end)
+					return Math.Max(stones[start], stones[end]);
+
+				int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];
+				int scoreRemoveLast = prefixSum[end] - prefixSum[start];
+
+				memo[start,end] = Math.Max(
+					scoreRemoveFirst - findDifference(start + 1, end, stones),
+					scoreRemoveLast - findDifference(start, end - 1, stones));
+
+				return memo[start,end];
+			}
+
+			public int StoneGameVII(int[] stones)
+			{
+				int n = stones.Length;
+				memo = new int[n,n];
+				prefixSum = new int[n + 1];
+				for (int i = 0; i < n; i++)
+				{
+					prefixSum[i + 1] = prefixSum[i] + stones[i];
+				}
+				return findDifference(0, n - 1, stones);
+			}
+		}
+
+
+
+		/// <summary>
+		/// 這跟DP2 一樣  已經可以命名為
+		/// Approach 2: Top Down Dynamic Programming - Memoization
+		///
+		/// 但是更聰明   我是直接把原寫法  原位置改寫
+		/// 但是  他更直接看出更簡化的地方  其實只要在  開頭  和結尾  做cache  位置下的比我直接改得更好
+		///
+		/// 本機測就進到了 20ms
+		///
+		/// 光是這樣就進入了
+		/// Runtime: 256 ms, faster than 63.64% of C# online submissions for Stone Game VII.
+		/// Memory Usage: 45.4 MB, less than 68.18% of C# online submissions for Stone Game VII.
+		///
+		/// 這range有點大  多送幾次會出現
+		/// Runtime: 240 ms, faster than 81.82% of C# online submissions for Stone Game VII.
+		/// Memory Usage: 45.4 MB, less than 63.64% of C# online submissions for Stone Game VII.
+		///
+		/// 
+		/// </summary>
+		class Solution_V2_Cache_DP3
+		{
+			int[] prefixSum;
+			int[,] memo;
+
+			private int findDifference(int start, int end, bool alice)
+			{
+				if (start == end)
+				{
+					return 0;
+				}
+				if (memo[start,end] != 0)
+				{
+					return memo[start,end];
+				}
+				int difference;
+				int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];
+				int scoreRemoveLast = prefixSum[end] - prefixSum[start];
+
+				if (alice)
+				{
+					difference = Math.Max(
+						findDifference(start + 1, end, !alice) + scoreRemoveFirst,
+						findDifference(start, end - 1, !alice) + scoreRemoveLast);
+				}
+				else
+				{
+					difference = Math.Min(
+						findDifference(start + 1, end, !alice) - scoreRemoveFirst,
+						findDifference(start, end - 1, !alice) - scoreRemoveLast);
+				}
+				memo[start,end] = difference;
+				return difference;
+			}
+
+			public int StoneGameVII(int[] stones)
+			{
+				int n = stones.Length;
+				memo = new int[n,n];
+				//for (int[] arr : memo)
+				//{
+				//	Arrays.fill(arr, INF);
+				//}
+				prefixSum = new int[n + 1];
+				for (int i = 0; i < n; i++)
+				{
+					prefixSum[i + 1] = prefixSum[i] + stones[i];
+				}
+				return Math.Abs(findDifference(0, n - 1, true));
+			}
+		}
+
+
+		/// <summary>
+		/// 唯一想到  可以再試的  Dic已經是頂級快了
+		/// 能在更快  可能只有 陣列 ?
+		/// 來試試
+		///
+		/// 不可思議   竟然只需要 40ms  這竟然Dictionary這種存取O(1)的  還能再快
+		/// 而且快了  15 倍
+		/// 沒有最快  只有更快  奇蹟啊
+		/// 陣列簡單  但是 超快
+		///
+		/// nxn的二維陣列
+		///
+		/// 這樣就pass了
+		///
+		/// Runtime: 540 ms, faster than 13.64% of C# online submissions for Stone Game VII.
+		/// Memory Usage: 49.3 MB, less than 18.18% of C# online submissions for Stone Game VII.
+		///
+		/// 還算慢  快的應該都 抄 sol吧
+		/// 像是底下的 Approach 5  184ms
+		///
+		/// /Time Complexity : \mathcal{O}(2^{n^{2}})O(2 
+		///	n 
+		///	2
+
+		///	). For an array of length nn, there are roughly { n^{ 2} }
+		///n 
+		///2
+		///possible subarrays.
+
+		///We fill the array prefixSum of size n by iterating n times.The time complexity would be \mathcal{ O}(n))O(n)).
+
+		///For each subarray there are 22 choices, either remove the first element or remove the last element.Thus the time complexity of recursive function would be \mathcal{ O}(2^{n^{2}})O(2 
+		///n
+		///2
+
+		///).
+
+		///This would give us total time complexity as \mathcal{O
+		///}(n)) + \mathcal{O}(2^{n^{2}}) = \mathcal{O}(2^{n^{2}})O(n))+O(2 
+		///n
+		///2
+
+		///)=O(2 
+		///n
+		///2
+
+		///).
+
+		///This approach is exhaustive and results in Time Limit Exceeded(TLE)
+
+		///Space Complexity: \mathcal{O}(n)O(n), as we build an array prefixSum of size nn.
+		///
+		/// 
+		/// </summary>
+		class Solution_V2_Cache_DP2
+		{
+			public static int cou = 0;
+			private int findDifference(int start, int end, bool alice)
+			{
+				if (start == end)
+				{
+					return 0;
+				}
+				int difference;       //因為ps 1~n 傳入的end是n-1 所以要+1轉為n
+				int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];// pn - p1
+				int scoreRemoveLast = prefixSum[end] - prefixSum[start];//剛看會以為-p0怪  但是對照一下Sol的圖  會發現這是真的  -p0才有保留1st元素的效果
+
+				int num1 = 0, num2 = 0;
+				Action findDiff = () =>
+				{
+					if (findDifferenceCache[start + 1, end] == 0)
+					{
+						num1 = findDifference(start + 1, end, !alice);//兩個區間就是這樣沒錯  起訖個往左或往右移一位
+						findDifferenceCache[start + 1, end] = num1;
+					}
+					else
+						num1 = findDifferenceCache[start + 1, end];
+
+					if (findDifferenceCache[start, end - 1] == 0)
+					{
+						num2 = findDifference(start, end - 1, !alice);
+						findDifferenceCache[start, end - 1] = num2;
+					}
+					else
+						num2 = findDifferenceCache[start, end - 1];
+				};
+
+				if (alice)
+				{
+					findDiff();
+					int num1SRF = num1 + scoreRemoveFirst;//兩個區間就是這樣沒錯  起訖個往左或往右移一位
+					int num2SRL = num2 + scoreRemoveLast;
+
+					difference = Math.Max(num1SRF, num2SRL);
+				}
+				else
+				{
+					findDiff();
+					int num1SRF = num1 - scoreRemoveFirst;//兩個區間就是這樣沒錯  起訖個往左或往右移一位
+					int num2SRL = num2 - scoreRemoveLast;
+
+					difference = Math.Min(num1SRF, num2SRL);
+				}
+				return difference;
+			}
+
+			int[] prefixSum;
+			int[,] findDifferenceCache = null;
+
+			public int StoneGameVII(int[] stones)
+			{
+				int n = stones.Length;
+				prefixSum = new int[n + 1];
+				for (int i = 0; i < n; i++)
+				{
+					prefixSum[i + 1] = prefixSum[i] + stones[i];// 0 ~ n-1  轉換到 1~n  
+				}
+
+				findDifferenceCache = new int[n, n];
+
+				int res = Math.Abs(findDifference(0, n - 1, true));
+				return res;
+			}
+		}
 
 
 
@@ -42,76 +295,75 @@ namespace YangPracticeLeetCode.Solved
 		/// 改一下
 		///
 		/// 確實又大幅進步  雖然TLE  但是已經 55/68 test case pass
-		/// 
+		/// 本地測 沒過的 case  570 ms
 		/// 
 		/// </summary>
-			class Solution_V2_Cache_DP
+		class Solution_V2_Cache_DP
+		{
+			public static int cou = 0;
+			private int findDifference(int start, int end, bool alice)
 			{
-				public static int cou = 0;
-				private int findDifference(int start, int end, bool alice)
+				if (start == end)
 				{
-					if (start == end)
+					return 0;
+				}
+				int difference;       //因為ps 1~n 傳入的end是n-1 所以要+1轉為n
+				int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];// pn - p1
+				int scoreRemoveLast = prefixSum[end] - prefixSum[start];//剛看會以為-p0怪  但是對照一下Sol的圖  會發現這是真的  -p0才有保留1st元素的效果
+
+				int num1 = 0, num2 = 0;
+				Action findDiff = () =>
+				{
+					if (!findDifferenceCache.ContainsKey($"{start + 1}_{end}"))
 					{
-						return 0;
-					}
-					int difference;       //因為ps 1~n 傳入的end是n-1 所以要+1轉為n
-					int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];// pn - p1
-					int scoreRemoveLast = prefixSum[end] - prefixSum[start];//剛看會以為-p0怪  但是對照一下Sol的圖  會發現這是真的  -p0才有保留1st元素的效果
-					string p = alice ? "A" : "B";
-
-					int num1=0, num2 = 0;
-					Action findDiff = () =>
-					{
-						if (!findDifferenceCache.ContainsKey($"{start + 1}_{end}"))
-						{
-							num1 = findDifference(start + 1, end, !alice);//兩個區間就是這樣沒錯  起訖個往左或往右移一位
-							findDifferenceCache.Add($"{start + 1}_{end}", num1);
-						}
-						else
-							num1 = findDifferenceCache[$"{start + 1}_{end}"];
-
-						if (!findDifferenceCache.ContainsKey($"{start}_{end - 1}"))
-						{
-							num2 = findDifference(start, end - 1, !alice);
-							findDifferenceCache.Add($"{start}_{end - 1}", num2);
-						}
-						else
-							num2 = findDifferenceCache[$"{start}_{end - 1}"];
-					};
-
-					if (alice)
-					{
-						findDiff();
-						int num1SRF = num1 + scoreRemoveFirst;//兩個區間就是這樣沒錯  起訖個往左或往右移一位
-						int num2SRL = num2 + scoreRemoveLast;
-
-						difference = Math.Max(num1SRF, num2SRL);
+						num1 = findDifference(start + 1, end, !alice);//兩個區間就是這樣沒錯  起訖個往左或往右移一位
+						findDifferenceCache.Add($"{start + 1}_{end}", num1);
 					}
 					else
-					{
-						findDiff();
-						int num1SRF = num1 - scoreRemoveFirst;//兩個區間就是這樣沒錯  起訖個往左或往右移一位
-						int num2SRL = num2 - scoreRemoveLast;
+						num1 = findDifferenceCache[$"{start + 1}_{end}"];
 
-						difference = Math.Min(num1SRF, num2SRL);
+					if (!findDifferenceCache.ContainsKey($"{start}_{end - 1}"))
+					{
+						num2 = findDifference(start, end - 1, !alice);
+						findDifferenceCache.Add($"{start}_{end - 1}", num2);
 					}
-					return difference;
+					else
+						num2 = findDifferenceCache[$"{start}_{end - 1}"];
+				};
+
+				if (alice)
+				{
+					findDiff();
+					int num1SRF = num1 + scoreRemoveFirst;//兩個區間就是這樣沒錯  起訖個往左或往右移一位
+					int num2SRL = num2 + scoreRemoveLast;
+
+					difference = Math.Max(num1SRF, num2SRL);
+				}
+				else
+				{
+					findDiff();
+					int num1SRF = num1 - scoreRemoveFirst;//兩個區間就是這樣沒錯  起訖個往左或往右移一位
+					int num2SRL = num2 - scoreRemoveLast;
+
+					difference = Math.Min(num1SRF, num2SRL);
+				}
+				return difference;
+			}
+
+			int[] prefixSum;
+			public Dictionary<string, int> findDifferenceCache = new Dictionary<string, int>();
+
+			public int StoneGameVII(int[] stones)
+			{
+				int n = stones.Length;
+				prefixSum = new int[n + 1];
+				for (int i = 0; i < n; i++)
+				{
+					prefixSum[i + 1] = prefixSum[i] + stones[i];// 0 ~ n-1  轉換到 1~n  
 				}
 
-				int[] prefixSum;
-				public Dictionary<string, int> findDifferenceCache = new Dictionary<string, int>();
-
-				public int StoneGameVII(int[] stones)
-				{
-					int n = stones.Length;
-					prefixSum = new int[n + 1];
-					for (int i = 0; i < n; i++)
-					{
-						prefixSum[i + 1] = prefixSum[i] + stones[i];// 0 ~ n-1  轉換到 1~n  
-					}
-
-					int res = Math.Abs(findDifference(0, n - 1, true));
-					return res;
+				int res = Math.Abs(findDifference(0, n - 1, true));
+				return res;
 			}
 		}
 
@@ -151,7 +403,7 @@ namespace YangPracticeLeetCode.Solved
 				int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];// pn - p1
 				int scoreRemoveLast = prefixSum[end] - prefixSum[start];//剛看會以為-p0怪  但是對照一下Sol的圖  會發現這是真的  -p0才有保留1st元素的效果
 				string p = alice ? "A" : "B";
-				Console.WriteLine($"{p} scoreRemoveFirst({start+1},{end+1}): {scoreRemoveFirst}  scoreRemoveLast({start},{end}): {scoreRemoveLast}");
+				Console.WriteLine($"{p} scoreRemoveFirst({start + 1},{end + 1}): {scoreRemoveFirst}  scoreRemoveLast({start},{end}): {scoreRemoveLast}");
 
 				if (alice)
 				{
@@ -162,7 +414,7 @@ namespace YangPracticeLeetCode.Solved
 					int num2SRL = num2 + scoreRemoveLast;
 
 					difference = Math.Max(num1SRF, num2SRL);
-					Console.WriteLine($"A, s1n  diff from {start +1} to {end}  is {num1} ,  +scoreRemoveFirst:{scoreRemoveFirst} = {num1SRF}");
+					Console.WriteLine($"A, s1n  diff from {start + 1} to {end}  is {num1} ,  +scoreRemoveFirst:{scoreRemoveFirst} = {num1SRF}");
 					Console.WriteLine($"A, se1 diff from {start } to {end - 1}  is {num2}, +scoreRemoveLast:{scoreRemoveLast} = {num2SRL} ");
 					Console.WriteLine($"Max = {difference}");
 				}
@@ -189,7 +441,7 @@ namespace YangPracticeLeetCode.Solved
 				{
 					prefixSum[i + 1] = prefixSum[i] + stones[i];// 0 ~ n-1  轉換到 1~n  
 				}
-				int res =  Math.Abs(findDifference(0, n - 1, true));
+				int res = Math.Abs(findDifference(0, n - 1, true));
 				Console.WriteLine($"res: {res}");
 				return res;
 			}
@@ -273,7 +525,7 @@ namespace YangPracticeLeetCode.Solved
 			{
 				int n = stones.Length;
 
-				int[,] dp = new int[n,n];
+				int[,] dp = new int[n, n];
 
 
 				int[] prefixSum = new int[n + 1];
@@ -288,12 +540,12 @@ namespace YangPracticeLeetCode.Solved
 						int end = start + length - 1;
 						int scoreRemoveFirst = prefixSum[end + 1] - prefixSum[start + 1];
 						int scoreRemoveLast = prefixSum[end] - prefixSum[start];
-						dp[start,end] = Math.Max(scoreRemoveFirst - dp[start + 1,end],
-							scoreRemoveLast - dp[start,end - 1]);
+						dp[start, end] = Math.Max(scoreRemoveFirst - dp[start + 1, end],
+							scoreRemoveLast - dp[start, end - 1]);
 
 					}
 				}
-				return dp[0,n - 1];
+				return dp[0, n - 1];
 			}
 		}
 
@@ -354,7 +606,7 @@ namespace YangPracticeLeetCode.Solved
 				//所以這裡寫到一半  就會自動想到改用雙指標更快
 				//while (ls.Any())
 				//{
-					
+
 				//}
 
 				bool roundA = true;
@@ -365,34 +617,34 @@ namespace YangPracticeLeetCode.Solved
 					{
 						//if (!roundA)
 						//{
-							if (s[i] < s[j])
-							{
-								sum -= s[i];
-								if (roundA)
-									A += sum;
-								else
-									B += sum;
-								i++;
-							}
-							else if (s[j] < s[i])
-							{
-								sum -= s[j];
-								if (roundA)
-									A += sum;
-								else
-									B += sum;
-								j--;
-							}
+						if (s[i] < s[j])
+						{
+							sum -= s[i];
+							if (roundA)
+								A += sum;
 							else
-							{
-								sum -= s[i];
-								if (roundA)
-									A += sum;
-								else
-									B += sum;
-								i++;
+								B += sum;
+							i++;
+						}
+						else if (s[j] < s[i])
+						{
+							sum -= s[j];
+							if (roundA)
+								A += sum;
+							else
+								B += sum;
+							j--;
+						}
+						else
+						{
+							sum -= s[i];
+							if (roundA)
+								A += sum;
+							else
+								B += sum;
+							i++;
 
-							}
+						}
 						//}
 						//else
 						//{
@@ -426,7 +678,7 @@ namespace YangPracticeLeetCode.Solved
 							int choose = i;
 							int i1 = i, j1 = j;
 							bool isChoose = false;
-							while (i1 + 1 < j1 -1)
+							while (i1 + 1 < j1 - 1)
 							{
 								if (s[i1 + 1] > s[j1 - 1])
 								{
